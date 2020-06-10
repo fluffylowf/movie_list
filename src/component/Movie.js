6b3d824c0f9749d1e6d9945b1905435b66d044b2
fluff <flufflowflying@gmail.com>
2020-06-11 03:01:52 +0900

    https://github.com/fluffylowf/movie_list

--- src/component/Movie.js
+++ src/component/Movie.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import prototype from 'prop-types';
+import proptype from 'prop-types';
 import {Link} from 'react-router-dom';
 ""
 function Movie({title, year, summary, poster}){
@@ -19,12 +19,12 @@ function Movie({title, year, summary, poster}){
     )
 }
 
-Movie.prototype = {
-    id: prototype.number.isRequired,
-    title: prototype.string.isRequired,
-    year: prototype.number.isRequired,
-    summary: prototype.string.isRequired,
-    poster: prototype.string.isRequired
+Movie.proptype = {
+    id: proptype.number.isRequired,
+    title: proptype.string.isRequired,
+    year: proptype.number.isRequired,
+    summary: proptype.string.isRequired,
+    poster: proptype.string.isRequired
 }
 
 export default Movie;
